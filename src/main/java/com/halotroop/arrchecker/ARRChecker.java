b6ede55d0b1403732515489488a1bbc0167b09f9
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2021-09-10 23:53:39 +0200

    clarify logs by switching from out to Log4j Logger

--- src/main/java/com/halotroop/arrchecker/ARRChecker.java
+++ src/main/java/com/halotroop/arrchecker/ARRChecker.java
@@ -7,9 +7,12 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.ModContainer;
 import net.fabricmc.loader.api.metadata.ModMetadata;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ARRChecker implements ModInitializer {
     private static final List<String> arrMods = new ArrayList<>();
+    private static final Logger LOGGER = LogManager.getLogger("arrchecker");
 
     @Override
     public void onInitialize() {
@@ -24,11 +27,11 @@ public class ARRChecker implements ModInitializer {
             if (!modMeta.getId().equals("minecraft")) {
                 if (modLicense.isEmpty()) {
                     // If no license is found, assume mod is not correctly licensed, and therefore, not modpack-friendly.
-                    System.out.println(modNameAndID + " has no license! It may be ARR!");
+                    LOGGER.warn(modNameAndID + " has no license! It may be ARR!");
                     arrMods.add(modMeta.getId());
                 } else if (modLicense.equals("all rights reserved") || modLicense.equals("arr") || modLicense.contains("copyright")) {
                     // If license is All Rights Reserved, or copyright is attributed, assume mod is not modpack-friendly.
-                    System.out.println(modNameAndID + " is ARR. Do not use it( in a modpack)!");
+                    LOGGER.warn(modNameAndID + " is ARR. Do not use it( in a modpack)!");
                     arrMods.add(modMeta.getId());
                 } else {
                     String[] validLicenses =
@@ -43,7 +46,7 @@ public class ARRChecker implements ModInitializer {
                     }
                     if (modLicenseInvalid) {
                         // If a valid license is not found, print crayon warning.
-                        System.out.println(modNameAndID + " may have a crayon license! It is: " + modLicense);
+                        LOGGER.warn(modNameAndID + " may have a crayon license! It is: " + modLicense);
                         // If a valid license is not found, assume mod is not modpack-friendly.
                         arrMods.add(modMeta.getId());
                     }
@@ -61,12 +64,11 @@ public class ARRChecker implements ModInitializer {
                 invalidMods.append(mod);
             }
             if (invalidMods.length() != 0) {
-                System.out.println(arrMods.size() == 1 ? "This mod" : "These mods" +
+                LOGGER.error(arrMods.size() == 1 ? "This mod" : "These mods" +
                         " may not be suitable for a modpack:\n" + invalidMods);
             }
         }
-        System.out.println("Minecraft isn't a mod, but in case you didn't know, it's ARR!");
-        System.out.println("Don't distribute it!");
+        LOGGER.warn("Minecraft isn't a mod, but in case you didn't know, it's ARR!\nDon't distribute it!");
     }
 
     public static List<String> getARRMods() {
